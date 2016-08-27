ba94204cb67b26328db33bc876259f767df438e1
Alexandre Wajcman <awajcman@gmail.com>
2016-08-27 17:17:20 -0300

    Remoção do PickerView com lista de Caroneiros

--- Caronas/CaronaViewController.swift
+++ Caronas/CaronaViewController.swift
@@ -11,7 +11,7 @@ import MapKit
 
 
 
-class CaronaViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CLLocationManagerDelegate {
+class CaronaViewController: UIViewController, CLLocationManagerDelegate {
 
     
     //MARK: - Outlets
@@ -23,7 +23,6 @@ class CaronaViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
    
     
     //MARK: - Properties
-    var arrayCaroneiros = [[String: String]]()
     
     let gps = CLLocationManager()
     
@@ -43,8 +42,7 @@ class CaronaViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         self.dataLabel.text = formatadorData.stringFromDate(data)
         
         //Ajustando PickerView
-        self.passageirosPickerView.dataSource = self
-        self.passageirosPickerView.delegate = self
+        
         
         //Ajustando o GPS
         self.gps.desiredAccuracy = kCLLocationAccuracyBestForNavigation
@@ -64,66 +62,14 @@ class CaronaViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         
         
     }
-    override func viewDidAppear(animated: Bool) {
-        
-        dispatch_async(dispatch_get_main_queue()) {
-            self.passageirosPickerView.reloadComponent(0)
-        }
-        
-        if NSFileManager.defaultManager().fileExistsAtPath(arquivo){
-            self.arrayCaroneiros = (NSArray(contentsOfFile: arquivo) as! [[String: String]])
-            if self.arrayCaroneiros.isEmpty {
-                let semCaroneiros = UIAlertController(title: "Alerta", message: "Não há passageiros cadastrados", preferredStyle: .Alert)
-                let acaoOK = UIAlertAction(title: "OK", style: .Default, handler: { (acaoOK) in
-                    print("Nao há passageiros cadastrados")
-                })
-                
-                semCaroneiros.addAction(acaoOK)
-                self.presentViewController(semCaroneiros, animated: true, completion: nil)
-                
-            }else {
-                print(self.arrayCaroneiros)
-            }
-        }else {
-            let semCaroneiros = UIAlertController(title: "Alerta", message: "Não há passageiros cadastrados", preferredStyle: .Alert)
-            let acaoOK = UIAlertAction(title: "OK", style: .Default, handler: { (acaoOK) in
-                print("Nao há passageiros cadastrados")
-            })
-            
-            semCaroneiros.addAction(acaoOK)
-            self.presentViewController(semCaroneiros, animated: true, completion: nil)
-        }
-        
-    }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     //MARK: - Actions
     
-    @IBAction func adicionarPassageiro(sender: UIButton) {
-    
-    // adionar passageiros a carona
-
-    }
-    
-    //MARK: - Métodos de PickerView DataSource
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return self.arrayCaroneiros.count
-    }
-    
-    
-    //MARK: - Métodos de PickerView Delegate
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return self.arrayCaroneiros[row]["nome"]
-    }
-
+   
     //MARK: - Métodos de LocationManager Delegate
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
